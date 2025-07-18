195b8b9a3a740deab332ee7380442d2b24ba80be
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-18 11:00:31 +0200

    'First'

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+//onsole.log('Hello World')
+function changeAllArticleColors() {
+    const articles=document.querySelectorAll('article')
+    for(let article of articles){
+        article.classList.add('sale')
+    }
+    console.log(article.classList)
+  // ... function body
+}
+
+changeAllArticleColors()
