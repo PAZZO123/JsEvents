2fa287eaf567a2dc2845874fb7ddb04d0ab4e85f
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-18 11:05:58 +0200

    'First'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function changeAllArticleColors() {
     const articles=document.querySelectorAll('article')
     for(let article of articles){
         article.classList.add('sale')
-        article.style.backgroundColor='puple'
+        article.style.backgroundColor=' #ffddd2'
     }
     console.log(article.classList)
   // ... function body
