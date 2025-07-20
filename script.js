a240d0088a8bb330bd7286208572855c67d0ea43
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-20 11:25:41 +0200

    'First'

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function attachBuyEvents() {
     let grandTotal=0
     button.addEventListener('click',function(e){ // . adding event listener to the parent
        
-        if(e.target.tagName==='BUTTON'){ //gettin button target from the parent
+        if(e.target.tagName==='BUTTON'){ //.gettin button target from the parent
             let product=e.target.closest('article')//trying to get the target which is closest or near to the target
             let row=document.createElement('tr')
             let price=parseFloat(product.querySelector('p').textContent)
